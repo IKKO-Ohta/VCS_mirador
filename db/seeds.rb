0d7b904acb1d6b734c22f30e1c1aa9024d8f9472
Ikko <ota.ikko.48v@st.kyoto-u.ac.jp>
2017-04-13 06:07:21 +0000

    fix db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: ture)
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
