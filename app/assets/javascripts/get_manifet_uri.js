49eac8f5d22109f204d1379acc0a6dc3bf872f45
Dion_M_Sato <dion.sato@gmail.com>
2017-04-17 23:11:58 +0900

    Adding variation request.

--- app/assets/javascripts/get_manifet_uri.js
+++ app/assets/javascripts/get_manifet_uri.js
@@ -1,4 +1,14 @@
+// This script will be embeded in html file for the page opens openseadragon.
+
+// html
+// <form name="hoge">
+// URL: <input type="text" name="ClientUrl"/>
+// <input type="submit" value="GetJson"
 // Gettting manifest_uri
+var request = new window.XMLhttpRequest();
+request.open('GET', '', true);
+request.send(null);
+
 // Getting sequence/canvas/images/id
 //Removing the specified data fetting IIIF Image API
 // Adding info.json
