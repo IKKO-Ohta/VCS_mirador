3fe203d2959e97a7f0d32701ca18e3633e27256a
Dion_M_Sato <dion.sato@gmail.com>
2017-04-17 12:40:34 +0900

    Change the comment
    豆腐百珍 for test json

--- app/assets/javascripts/get_manifet_uri.js
+++ app/assets/javascripts/get_manifet_uri.js
@@ -1,17 +1,4 @@
 // Gettting manifest_uri
-
 // Getting sequence/canvas/images/id
-JSON.parse('{"p": 5}', function(k, v) {
-  if (typeof v === 'number') {
-    return v * 2;  // 数字については v * 2 を返す
-  }
-  return v;        // 他のすべてはそのまま返す
-});
-
-JSON.parse('{"1": 1, "2": 2, "3": {"4": 4, "5": {"6": 6}}}', function(k, v) {
-  console.log(v); // 現在のプロパティ名を出力する。最後は ""。
-  return k;       // 変更されていないプロパティの値を返す。
-});
-
 //Removing the specified data fetting IIIF Image API
 // Adding info.json
