7b8a949543521209d399922fcede8d588876f0e5
Dion_M_Sato <dion.sato@gmail.com>
2017-04-17 10:35:51 +0900

    Insert json for test.

--- app/assets/javascripts/get_manifet_uri.js
+++ app/assets/javascripts/get_manifet_uri.js
@@ -1,12 +1,17 @@
 // Gettting manifest_uri
 
 // Getting sequence/canvas/images/id
-JSON.parse('{"@id" : "http://gallica.bnf.fr/iiif/ark:/12148/btv1b83018108/f1"}', function(k, v){
-  if (k === 'string'){
-    console.log(k);
-    return v;
-    }
-  });
+JSON.parse('{"p": 5}', function(k, v) {
+  if (typeof v === 'number') {
+    return v * 2;  // 数字については v * 2 を返す
+  }
+  return v;        // 他のすべてはそのまま返す
+});
+
+JSON.parse('{"1": 1, "2": 2, "3": {"4": 4, "5": {"6": 6}}}', function(k, v) {
+  console.log(v); // 現在のプロパティ名を出力する。最後は ""。
+  return k;       // 変更されていないプロパティの値を返す。
+});
 
 //Removing the specified data fetting IIIF Image API
 // Adding info.json
