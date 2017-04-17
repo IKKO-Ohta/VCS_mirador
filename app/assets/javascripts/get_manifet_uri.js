3ac828ca3d25902843eea4cb9e90c6a816766555
Dion_M_Sato <dion.sato@gmail.com>
2017-04-18 08:58:45 +0900

    Adding event request

--- app/assets/javascripts/get_manifet_uri.js
+++ app/assets/javascripts/get_manifet_uri.js
@@ -1,13 +1,26 @@
 // This script will be embeded in html file for the page opens openseadragon.
 
-// html
-// <form name="hoge">
-// URL: <input type="text" name="ClientUrl"/>
-// <input type="submit" value="GetJson"
+// ___________html__________
+// <form name="GetUrl">
+// Fetch URL: <input type="text" name="Url"/>
+// <input type="submit" value="Get" onclick="GetManifestUri">
+// <script src="get_manifest_uri.js"</script>
+// ___________________________
+
 // Gettting manifest_uri
-var request = new window.XMLhttpRequest();
-request.open('GET', '', true);
-request.send(null);
+var name = document.form.name.value;
+document.form.name.value = 'Getjson';
+encodeURIComponent(Getjson);
+function GetManifestUri() {
+  var request = new window.XMLhttpRequest();
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 404 || 500){
+      alert("This URL is unverified.");
+  }
+  request.open('GET', 'Getjson?param=' + Math.random(), true);
+  request.send(null);
+  }
+}
 
 // Getting sequence/canvas/images/id
 //Removing the specified data fetting IIIF Image API
