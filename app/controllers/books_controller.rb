e0aa615030ee542b56029a5a191a985088f4ddc7
Ikko <ota.ikko.48v@st.kyoto-u.ac.jp>
2017-04-22 12:51:59 +0000

    implement book request

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,20 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def create
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "Request accepted!!"
+      redirect_to @book
+    else
+      render 'Oops...'
+    end
+  end
+
   def show
     @book = Book.find(params[:id])
     @comment = @book.comments.build if logged_in?
@@ -9,6 +23,7 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
   def delete
   end
@@ -16,4 +31,11 @@ class BooksController < ApplicationController
   end
   def update
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :uri, :isLocked)
+  end
+
 end
