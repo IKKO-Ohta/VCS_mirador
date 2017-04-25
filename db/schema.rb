3432265bec00f0cc4dc448127fc744b93e33d151
Ikko <ota.ikko.48v@st.kyoto-u.ac.jp>
2017-04-25 03:17:43 +0000

    in postgre

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170423155829) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.string   "uri"
@@ -30,8 +33,8 @@ ActiveRecord::Schema.define(version: 20170423155829) do
     t.boolean  "deleted"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["book_id", "created_at"], name: "index_comments_on_book_id_and_created_at"
-    t.index ["book_id"], name: "index_comments_on_book_id"
+    t.index ["book_id", "created_at"], name: "index_comments_on_book_id_and_created_at", using: :btree
+    t.index ["book_id"], name: "index_comments_on_book_id", using: :btree
   end
 
   create_table "microposts", force: :cascade do |t|
@@ -40,8 +43,8 @@ ActiveRecord::Schema.define(version: 20170423155829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
-    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_microposts_on_user_id"
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at", using: :btree
+    t.index ["user_id"], name: "index_microposts_on_user_id", using: :btree
   end
 
   create_table "relationships", force: :cascade do |t|
@@ -49,9 +52,9 @@ ActiveRecord::Schema.define(version: 20170423155829) do
     t.integer  "followed_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+    t.index ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,7 +65,9 @@ ActiveRecord::Schema.define(version: 20170423155829) do
     t.string   "password_digest"
     t.boolean  "admin",           default: false
     t.string   "remember_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "books"
+  add_foreign_key "microposts", "users"
 end
